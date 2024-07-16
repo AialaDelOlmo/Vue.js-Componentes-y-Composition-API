ad91351c18679bc4c660b0e1b6bdf1567ed85b6f
Aiala Del Olmo <aialadelolmo@gmail.com>
2024-07-16 13:12:12 +0200

    uso de ref

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,16 @@
 <template>
-    <div>Hola</div>
+    <div>{{ counter }}</div>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
     setup() {
-       
+        const counter = ref(0);
+        setInterval(() => counter.value++, 1000);
+        return { 
+            counter,
+        };
     },
 };
 </script>
