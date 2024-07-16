b35b3d224303ce9815bbb60bfccfbb08fc915dd8
Aiala Del Olmo <aialadelolmo@gmail.com>
2024-07-16 15:57:31 +0200

    Watchers

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,19 @@
 <template>
-    <div>{{ obj }}</div>
+    <div>{{ obj.counter }}</div>
 </template>
 
 <script>
 //import { ref } from 'vue';
-import { reactive } from 'vue';
+import { reactive, watch } from 'vue';
 export default {
     setup() {
         //const counter = ref(0);
         const obj = reactive({ counter: 0 });
         setInterval(() => obj.counter++, 1000);
+
+        watch(() => obj.counter, (valor, anterior) => {
+            console.log('El valor del contador ha cambiado: ', valor, 'antes: ', anterior);
+        });
         return { 
             obj,
         };
